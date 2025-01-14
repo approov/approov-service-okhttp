af023c4ae625224f02c44fabbf2f5662d66b13c6
jexh <jexh@users.noreply.github.com>
2025-01-14 20:56:09 +0000

    Update ApproovMessageSigningConfig.java
    Proposed changes that will need to be discussed before being adopted in a final form.

--- approov-service/src/main/java/io/approov/service/okhttp/ApproovMessageSigningConfig.java
+++ approov-service/src/main/java/io/approov/service/okhttp/ApproovMessageSigningConfig.java
@@ -1,6 +1,18 @@
 package io.approov.service.okhttp;
 
 import java.util.ArrayList;
+import okhttp3.Request;
+
+/* Add the following interfaces */
+interface MessageSigningConfig {
+    String getSigningMessage();
+    String getTargetHeaderName();
+    String generateTargetHeaderValue(String messageSignature, String approovTokenHeader);
+}
+
+interface MessageSigningConfigFactory {
+    MessageSigningConfig generateMessageSigningConfig(Request request, String approovTokenHeader);
+}
 
 /* message signing configuration
 * This class is used to configure the message signing feature. The message signature can be computed based on the
@@ -9,29 +21,18 @@ import java.util.ArrayList;
 * You can have multiple configurations for different domains and a default '*' configuration that will be used if no
 * specific configuration is found for a domain.
 */
-public class ApproovMessageSigningConfig {
+public class DefaultMessageSigningConfigFactory implements MessageSigningConfigFactory {
     // the name of the header that will be used to send the message signature
     protected String targetHeader;
     // the list of headers to include in the message to be signed, in the order they should be added
     protected ArrayList<String> signedHeaders;
-    // true if the message body should also be signed
-    protected Boolean signBody;
-    // true if the Approov token header is included in the signature
-    protected Boolean signApproovToken;
-    // true if target URL should be included in the signature
-    protected Boolean signURL;
-    // true if the network request method should be included in the signature
-    protected Boolean signMethod;
+
     // constructor
     public ApproovMessageSigningConfig(String targetHeader) {
         if (targetHeader == null || targetHeader.isEmpty())
             throw new IllegalArgumentException("The target header must be specified");
         this.targetHeader = targetHeader;
         this.signedHeaders = new ArrayList<>();
-        this.signBody = false;
-        this.signApproovToken = true;
-        this.signURL = true;
-        this.signMethod = true;        
     }
 
     /* Get/set methods */
@@ -40,48 +41,85 @@ public class ApproovMessageSigningConfig {
     public String getTargetHeader() {
         return targetHeader;
     }
-    /* Get signed headers */
-    public ArrayList<String> getSignedHeaders() {
-        return signedHeaders;
-    }
+
     /* Add a header to the list of signed headers: NOTE the sequence of headers DOES matter */
-    public void addSignedHeader(String header) {
+    public DefaultMessageSigningConfigFactory addSignedHeader(String header) {
         if (header == null || header.isEmpty())
             throw new IllegalArgumentException("The header must be specified");
         signedHeaders.add(header);
-    }
-    /* Get signBody flag */
-    public Boolean getSignBody() {
-        return signBody;
-    }
-    /* Set signBody flag */
-    public void setSignBody(Boolean signBody) {
-        this.signBody = signBody;
-    }
-    /* Get signApproovToken flag */
-    public Boolean getSignApproovToken() {
-        return signApproovToken;
-    }
-    /* Set signApproovToken flag */
-    public void setSignApproovToken(Boolean signApproovToken) {
-        this.signApproovToken = signApproovToken;
-    }
-    /* Get signURL flag */
-    public Boolean getSignURL() {
-        return signURL;
-    }
-    /* Set signURL flag */
-    public void setSignURL(Boolean signURL) {
-        this.signURL = signURL;
-    }
-    /* Get signMethod flag */
-    public Boolean getSignMethod() {
-        return signMethod;
-    }
-    /* Set signMethod flag */
-    public void setSignMethod(Boolean signMethod) {
-        this.signMethod = signMethod;
+        return this;
     }
 
+    MessageSigningConfig generateMessageSigningConfig(Request request, String approovTokenHeader){
+        StringBuilder usedHeadersSpec = new StringBuilder();
+        StringBuilder message = new StringBuilder();
+        // 1. Add the Method to the message
+        message.append(request.method());
+        message.append("\n");
+        //  2. add the URL to the message, followed by a newline
+        message.append(request.url()); // TODO make sure this includes all the URL params if there are any
+        message.append("\n");
+        // 3. add the Approov token header to the message
+        List<String> values = request.headers(approovTokenHeader); // make sure the okhtp lookup works whatever the case used on the header name
+        usedHeadersSpec.append(approovTokenHeader.toLowerCase() + "," + values.size())
+        for (String value : values) {
+            message.append(approovTokenHeader.toLowerCase()).append(":");
+            if (value != null) {
+                message.append(value);
+            }
+            message.append("\n");
+        }
 
+        // 4. add the required headers to the message as 'headername:headervalue', where the headername is in
+        // lowercase
+        if (messageSigningConfig.getSignedHeaders() != null) {
+            for (String header : messageSigningConfig.signedHeaders) {
+                // add one headername:headervalue\n entry for each header value to be included in the signature
+                List<String> values = request.headers(header);
+                if (values != null && values.length() > 0) {
+                    usedHeadersSpec.append(approovTokenHeader.toLowerCase()+ "," + values.size())
+                    for (String value : values) {
+                        message.append(header.toLowerCase()).append(":");
+                        if (value != null) {
+                            message.append(value);
+                        }
+                        message.append("\n");
+                    }
+                }
+            }
+        }
+        String finalUsedHeadersSpec = usedHeadersSpec.String()
+        message.append(finalUsedHeadersSpec)
+
+        // add the body to the message
+        okhttp3.RequestBody body = request.body();
+        if (body != null) {
+            Buffer buffer = new Buffer();
+            body.writeTo(buffer);
+            message.append(buffer.readUtf8());
+        }
+        return new DefaultMessageSigningConfig(
+        );
+    }
+}
+
+public class DefaultMessageSigningConfig implements MessageSigningConfig
+    // the name of the header that will be used to send the message signature
+    private String targetHeader;
+    // the list of headers with counts that are expected by the server and were also included in the message to be signed
+    private String usedHeadersSpec;
+    // the message to be signed
+    private String message;
+
+    DefaultMessageSigningConfig(String targetHeader, String usedHeadersSpec, String message) {
+        this.targeHeader = targetHeader;
+        this.usedHeaderSpec = usedHeaderSpec;
+        this.message = message;
+    }
+
+    public String getTargetHeaderName(){ return targetHeader }
+    public String getSigningMessage() { return usedHeadersSpec }
+    public String generateTargetHeaderValue(String messageSignature) {
+        return usedHeaderSpec + ";" + messageSignature;
+    }
 }
